f5b6686e1edc65a429996028a760891798a6d14d
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-01 09:32:55 +0100

    getters & setters

--- bin/main.dart
+++ bin/main.dart
@@ -3,6 +3,7 @@ import './person_class.dart';
 void main() {
   Person father = Person("Houcem Hedhly", "98765432", "Bizerte");
   print(father.name); // Houcem Hedhly
+  print(father.phone); // 98765432
 
   Person son = Person.kid("Haroun Hedhly");
   print(son.name); // Haroun Hedhly
--- bin/person_class.dart
+++ bin/person_class.dart
@@ -1,8 +1,15 @@
 class Person {
   String? name;
-  String? phone;
+  String? _phone;
   String? address;
 
-  Person(this.name, this.phone, this.address);
+  Person(this.name, this._phone, this.address);
   Person.kid(this.name);
+
+  // phone attribute getter & setter
+  String get phone => _phone.toString();
+
+  set phone(String value) {
+    _phone = value;
+  }
 }
