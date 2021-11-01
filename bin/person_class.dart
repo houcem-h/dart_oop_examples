7466c7f447c46acb38c99d40430b77a0fd0c3ba4
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-01 09:15:25 +0100

    person class first version

---
+++ bin/person_class.dart
@@ -0,0 +1,11 @@
+class Person {
+  String? name;
+  String? _phone;
+  String? address;
+
+  Person(String name, String phone, String address) {
+    this.name = name;
+    this._phone = phone;
+    this.address = address;
+  }
+}
