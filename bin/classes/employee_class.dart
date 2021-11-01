2c9e9ac249f3698de36f297a6a7b5450c350a483
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-01 10:32:30 +0100

    walker interface

--- bin/classes/employee_class.dart
+++ bin/classes/employee_class.dart
@@ -1,10 +1,17 @@
 import './person_class.dart';
+import './../interfaces/walker_interface.dart';
 
-class Employee extends Person {
+class Employee extends Person implements Walker {
   double salary;
 
   Employee(String name, String phone, this.salary) : super(name, phone);
 
+  @override
   String speak(String sentence) =>
       "Hi, this is an employee. he says: $sentence";
+
+  @override
+  void walk() {
+    print("Employee is walking");
+  }
 }
